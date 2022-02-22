17521d15fb35d2cb8e24c34dfe4c7ef6173b0860
Sam Sepial <zprestoz@protonmail.com>
2022-02-22 16:35:37 -0500

    Implemented basic functionality, when user clicks house sigil, will change the description and name displayed

--- app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
@@ -2,10 +2,24 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layout = findViewById<View>(R.id.layout)
+
+        val image = findViewById<ImageView>(R.id.arrynImage)
+        val imageName = findViewById<TextView>(R.id.imageName)
+        val imageDescription = findViewById<TextView>(R.id.imageDescription)
+        image.setOnClickListener {
+            imageName.text = "Hello there"
+            imageDescription.text = "This is the picture's description"
+        }
+
     }
 }
